6648f428eab4dafebae8ac9809067a81d9e68c7f
Katrina Payne <fullmetalharlot@nimhlabs.com>
2009-11-05 14:41:02 -0700

    Started work on this

---
+++ Source/Backend/machine.hxx
@@ -0,0 +1,13 @@
+
+/*
+ * SAGE Miss System
+ * File     : $MissSystem/Source/Backend/machine.hxx
+ * Authors  : Katrina Payne
+ * Creation : Octobre 5th 2009
+ * Groups   : NIMH Labs, Radd Team
+ * Abstract :
+ * This file contains header file information for use in a emulator using the 
+ * SAGE Miss System Framework
+ */
+
+
